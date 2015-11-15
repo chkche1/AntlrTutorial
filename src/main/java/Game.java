260571f8417f5fff9e3bc766beec1ce8b25f4fde
Kent Chen <chkche1@gmail.com>
2015-11-14 23:34:19 -0800

    Committing a working example

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,10 +1,10 @@
 import java.util.Map;
+import java.util.Scanner;
 
 public class Game {
-
-    private final String name;
-    private final Map<String, Integer> points;
-    private final String[][] grid;
+    private String name;
+    private Map<String,Integer> points;
+    private String[][] grid;
     private int score = 0;
 
     public Game(String name, Map<String, Integer> points, String[][] grid) {
@@ -13,4 +13,25 @@ public class Game {
         this.grid = grid;
     }
 
+    public void play() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("You're playing " + name + ".") ;
+
+        while (true) {
+            System.out.println("Where do you want to dig (enter x then y)?");
+
+            int x = in.nextInt();
+            int y = in.nextInt();
+
+            if (grid[x][y] != null) {
+                String treasure = grid[x][y];
+                score += points.get(treasure);
+                grid[x][y] = null;
+                System.out.println("You found " + treasure + "! Your score is " + score + ".");
+            } else {
+                System.out.println("Sorry, nothing there!");
+            }
+        }
+    }
 }
